b2a09d31a6e146bc21c14b272a9a6eec52fa350e
Max Larsson <maxslarsson@gmail.com>
2021-12-15 11:54:35 -0500

    Solved problem 3 of 2021 Feb Bronze

---
+++ Bronze/2021/Feb/3.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+string direction(string path) {
+
+    vector<char> cardinal_directions = {'N', 'E', 'S', 'W'};
+
+    auto itr = find(cardinal_directions.begin(), cardinal_directions.end(), path[0]);
+    int i;
+    if (itr != cardinal_directions.end()) {
+        i = itr - cardinal_directions.begin();
+    } else {
+        cerr << "Uh, oh!" << endl;
+    }
+
+    int count = 0;
+    for (auto c: path.substr(1, path.length() - 1)) {
+        int index1 = i - 1;
+        if (index1 == -1) index1 = cardinal_directions.size() - 1;
+
+        int index2 = i + 1;
+        if (index2 == cardinal_directions.size()) index2 = 0;
+
+        if (cardinal_directions[i] == c) {
+            continue;
+        } else if (cardinal_directions[index1] == c) {
+            i = index1;
+            count--;
+        } else if (cardinal_directions[index2] == c) {
+            i = index2;
+            count++;
+        } else {
+            cerr << "Char is not right before or after..." << endl;
+        }
+    }
+
+    if (count > 0) {
+        return "CW";
+    } else if (count < 0) {
+        return "CCW";
+    } else {
+        cerr << "Count is equal to 0..." << endl;
+        return "";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string path;
+        cin >> path;
+        cout << direction(path) << endl;
+    }
+
+    return 0;
+}
